c1e8fc1b56b22a7662e369309bd852c37acb8493
hasikiFire <hexz03@vanke.com>
2024-12-25 15:40:36 +0800

    feat: cors

---
+++ src/main/java/com/hasikiFire/networkmall/core/filter/CorsFilter.java
@@ -0,0 +1,29 @@
+package com.hasikiFire.networkmall.core.filter;
+
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "CorsFilter ")
+@Configuration
+public class CorsFilter implements Filter {
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) res;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers", "*");
+    chain.doFilter(req, res);
+  }
+}
