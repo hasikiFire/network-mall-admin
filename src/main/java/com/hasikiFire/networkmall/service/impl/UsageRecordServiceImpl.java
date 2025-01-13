3a4cf6a99ebbabbf6cbfb703ebb0e5288ce6d120
hasikiFire <hasikiFire@outlook.com>
2025-01-13 17:28:59 +0800

    feat: xxl-job

--- src/main/java/com/hasikiFire/networkmall/service/impl/UsageRecordServiceImpl.java
+++ src/main/java/com/hasikiFire/networkmall/service/impl/UsageRecordServiceImpl.java
@@ -13,6 +13,8 @@ import com.hasikiFire.networkmall.dto.resp.PackageListRespDto;
 import com.hasikiFire.networkmall.dto.resp.PackageRespDto;
 import com.hasikiFire.networkmall.dto.resp.UsageRecordDetailRespDto;
 import com.hasikiFire.networkmall.service.UsageRecordService;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
 
 import cn.dev33.satoken.stp.StpUtil;
 import jakarta.validation.Valid;
@@ -22,7 +24,13 @@ import lombok.extern.slf4j.Slf4j;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * <p>
@@ -126,6 +134,44 @@ public class UsageRecordServiceImpl extends ServiceImpl<UsageRecordMapper, Usage
       throw new RuntimeException("使用记录创建失败", e);
     }
     return usageRecord;
+  }
+
+  public static void addJob(String jobHandler, String cron, String param) {
+    // RestTemplate restTemplate = new RestTemplate();
+    // Map<String, Object> request = new HashMap<>();
+    // request.put("jobGroup", 2); // 执行器 ID
+    // request.put("jobDesc", "一个月后执行的任务");
+    // request.put("cron", cron);
+    // request.put("executorHandler", jobHandler);
+    // request.put("executorParam", param);
+
+    // String url = XXL_JOB_ADMIN_URL + "/jobinfo/add";
+    // restTemplate.postForObject(url, request, String.class);
+  }
+
+  @XxlJob("usageRecordJobHandler")
+  public void usageRecordJobHandler() throws Exception {
+
+    // if (targetDateStr == null || targetDateStr.isEmpty()) {
+    // XxlJobHelper.log("任务参数为空，请检查");
+    // return;
+    // }
+
+    // // 解析目标日期
+    // LocalDate targetDate = LocalDate.parse(targetDateStr,
+    // DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    // LocalDate currentDate = LocalDate.now();
+
+    // // 判断当前时间是否达到目标时间
+    // if (currentDate.isBefore(targetDate)) {
+    // XxlJobHelper.log("任务未到执行时间，跳过");
+    // return;
+    // }
+
+    // // 执行任务逻辑
+    // XxlJobHelper.log("任务开始执行，目标时间：" + targetDate);
+    // // TODO: 添加你的业务逻辑
+    // XxlJobHelper.log("任务执行完成");
   }
 
 }
